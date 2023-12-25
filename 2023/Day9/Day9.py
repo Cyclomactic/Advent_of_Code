dc7f3c1c7ec98aaede8c2409edd574051105704c
Cyclomactic <cyclomactic@gmail.com>
2023-12-25 13:10:11 -0600

    Day 9 part 2 complete

--- 2023/Day9/Day9.py
+++ 2023/Day9/Day9.py
@@ -2,7 +2,7 @@ import re
 from utils import timer
 
 
-file_v = 'sample'
+file_v = 'text'
 file = '2023/Day9/input/' + file_v + '.txt'
 
 @timer
@@ -15,23 +15,38 @@ def main():
               histories.append(ints)
     
     next_values = []
-    # history_plus_next_values = []
+    first_values = []
     for history in histories:
         last_sequence_digits = []
+        first_sequence_digits = []
         length = len(history)
         last_sequence_digits.append(history[length-1])
-        find_differences(history, last_sequence_digits)
+        find_differences(history, last_sequence_digits, first_sequence_digits)
         next_value = sum(last_sequence_digits)
         next_values.append(next_value)
-        # history_plus_next_values = list(history)
-        # history_plus_next_values.append(next_value)
-        # print(history_plus_next_values)
+        # part 2
+        first_sequence_digits.append(0)
+        len_fsd = len(first_sequence_digits)
+        fsd_index = -1
+        new_digit_pass = fsd_index
+        while (fsd_index*-1) < len_fsd:
+            if fsd_index == -1:
+                new_digit = first_sequence_digits[fsd_index-1] - first_sequence_digits[fsd_index]
+                new_digit_pass = new_digit
+                fsd_index -= 1
+            else:
+                new_digit = first_sequence_digits[fsd_index-1] - new_digit_pass
+                new_digit_pass = new_digit
+                fsd_index -= 1
+        first_values.append(history[0] - new_digit)
     
     sum_next_values = sum(next_values)
     print('Part 1 answer: ' + str(sum_next_values))
 
+    sum_first_values = sum(first_values)
+    print('Part 2 answer: ' + str(sum_first_values))
 
-def find_differences(sequence, last_sequence_digits):
+def find_differences(sequence, last_sequence_digits, first_sequence_digits):
     length = len(sequence)
     index = 0
     new_level = []
@@ -40,11 +55,12 @@ def find_differences(sequence, last_sequence_digits):
         new_level.append(difference)
         index += 1
     last_sequence_digits.append(difference)
+    first_sequence_digits.append(new_level[0])
     if new_level.count(new_level[0]) == len(new_level):
         return difference
     else:
         sequence = new_level
-        find_differences(sequence, last_sequence_digits)
+        find_differences(sequence, last_sequence_digits, first_sequence_digits)
     return difference
 
 
