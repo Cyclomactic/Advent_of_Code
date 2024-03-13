23191502a6cdad84950e61621420adf1dc708e10
Cyclomactic <cyclomactic@gmail.com>
2024-03-13 17:14:31 -0500

    Day 1 solution

--- 2015/Day1/Day1.py
+++ 2015/Day1/Day1.py
@@ -1,13 +1,23 @@
 import linecache
 INPUT = "./2015/Day1/input/input.txt"
 a = linecache.getline(INPUT, 1)
+
+# part 1
 opn = a.count('(')
 cls = a.count(')')
 floor = opn - cls
 print('floor: ' + str(floor))
 
 # part 2
-b = ", ".join(a[i:i+1] for i in range(0, len(a), 1))
-c = b[:-3]
-d = "'" + c + "'"
-res = eval(d)
+floor = 0
+position = 0
+for char in a:
+    if char == "(":
+        floor += 1
+    elif char == ")":
+        floor -= 1
+    position += 1
+    print(f'position: {position}')
+    if floor == -1:
+        print(f'position: {position}')
+        break
