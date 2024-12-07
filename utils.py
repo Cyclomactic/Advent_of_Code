c99eb1e72a9b36786cbc62203e889f23b91ca055
Cyclomactic <cyclomactic@gmail.com>
2024-12-06 22:42:46 -0800

    added color func for prints

--- utils.py
+++ utils.py
@@ -11,3 +11,9 @@ def timer(func):
             t2 = time()
             print(f'Function {func.__name__!r} executed in {(t2-t1): .4f}s')
     return wrap_function
+
+
+def color(color_num):
+    # Black: 30, Red: 31, Green: 32, Yellow: 33
+    # Blue: 34, Magenta: 35, Cyan: 36, White: 37
+    print(f'test test [{color_num}misThisRed?[0m test test')
