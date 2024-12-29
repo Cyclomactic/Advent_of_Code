c8ce4009e1b5d81dbcf8840f7397fcdcdefe3eb1
Cyclomactic <cyclomactic@gmail.com>
2024-12-29 07:48:08 -0800

    optimizing with @pkoziol

--- 2024/Day6/Day6.py
+++ 2024/Day6/Day6.py
@@ -4,12 +4,12 @@ from utils import timer, Point
 file_v = 'text'
 file = '2024/Day6/input/' + file_v + '.txt'
 
-directions = [
-    ('north', Point(0, -1)),
-    ('east', Point(1, 0)),
-    ('south', Point(0, 1)),
-    ('west', Point(-1, 0)),
-    ]
+north = Point(0, -1)
+east = Point(1, 0)
+south = Point(0, 1)
+west = Point(-1, 0)
+
+directions = [north, east, south, west]
 
 
 def parse_data(file):
@@ -20,49 +20,54 @@ def parse_data(file):
             lines.append([
                     char for char in line.strip()])
             if '^' in line:
-                orig_guard_y = y
-                orig_guard_x = line.index('^')
+                guard = Point(line.index('^'), y)
             y += 1
-    return lines, orig_guard_x, orig_guard_y
+    return lines, guard
 
 
-def move(cords, direction):
-    new_loc = cords + direction
-    return new_loc
+def move(guard_position, direction):
+    possible_position = guard_position + direction
+    return possible_position
 
 
-def cord_check(map, x, y, index, win=False, check=False):
-    len_map = len(map[0])
-    len_line = len(map[0][0])
-    if y < 0 or y >= len_map or x < 0 or x >= len_line:
-        win = True
-    elif map[0][y][x] == '#':
-        if index == 3:
-            index = 0
-        else:
-            index += 1
-        check = True
-    return win, index, check
-
-
-def part1(map):
+def cord_check(map, possible_position, dir_index):
     win = False
-    check = False
-    positions = []
-    cords = Point(map[1], map[2])
+    can_move_here = True
+    len_map = len(map)
+    len_line = len(map[0])
+    if (
+        possible_position.y < 0
+        or possible_position.y >= len_map
+        or possible_position.x < 0
+        or possible_position.x >= len_line
+    ):
+        win = True
+    elif map[possible_position.y][possible_position.x] == '#':
+        dir_index += 1
+        dir_index = dir_index % len(directions)
+        can_move_here = False
+    return win, dir_index, can_move_here
+
+
+def part1(map, guard_position):
+    win = False
+    can_move_here = True
+    unique_positions = set()
     dir_index = 0
     while win is False:
-        position = (cords.x, cords.y)
-        if position not in positions:
-            positions.append(position)
+        current_position = guard_position
+        if current_position not in unique_positions:
+            unique_positions.add(current_position)
         direction = directions[dir_index]
-        pot_cords = move(cords, direction[1])
-        check = cord_check(map, pot_cords.x, pot_cords.y, dir_index)
-        win = check[0]
-        if check[2] is False:
-            cords = pot_cords
-        dir_index = check[1]
-    print(f'unique positions: {len(positions)}')
+        possible_position = move(guard_position, direction)
+        win, dir_index, can_move_here = cord_check(
+            map,
+            possible_position,
+            dir_index
+        )
+        if can_move_here:
+            guard_position = possible_position
+    print(f'unique positions: {len(unique_positions)}')
 
 
 def part2():
@@ -71,9 +76,10 @@ def part2():
 
 @timer
 def main():
-    map = parse_data(file)
+    map, guard_position = parse_data(file)
 
-    part1(map)
+    part1(map, guard_position)
+    # print(f'counter: {counter}')
     part2()
 
 
